4f7978514303ee29709ec9ff456a3b1393e0cb5e
Stephen Rudge <sbrudge@comcast.net>
2023-02-04 13:02:35 -0600

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorPractice
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string HairColor { get; set; }
+        public int Age { get; set; }
+
+        public Person(string taco, string taco1, int taco2)
+        {
+            name = taco;
+            HairColor = taco1;
+            Age = taco2;
+        }
+        public Person()
+        {
+
+        } 
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// constructorPractice
+
+
+using ConstructorPractice;
+
+Person stephen = new Person();
+
+
+stephen.name = "Stephen";
+stephen.Age = 31;
+stephen.HairColor = "black";
+Console.WriteLine($"{stephen.name} is my name. I am {stephen.Age} years old and have {stephen.HairColor} hair. ");
+
+
+Person Jeremy= new Person("Jeremy", "Brown", 30);
+Console.WriteLine($"{Jeremy.name} is my name. I am {Jeremy.Age} years old and have {Jeremy.HairColor} hair. ");
+
+
